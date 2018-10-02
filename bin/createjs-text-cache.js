4986969ce4e172cefe63883342af5d12516a236e
MasatoMakino <unagiinu.g@gmail.com>
2018-10-02 12:18:24 +0900

    first commit

---
+++ bin/createjs-text-cache.js
@@ -0,0 +1,82 @@
+export class CreatejsCacheUtil {
+  /**
+   * フィルタ適用のためのキャッシュを生成する。
+   * @param {createjs.DisplayObject} target
+   * @param {createjs.Filter[]} filters
+   * @param {number} margin
+   */
+  static setFilter(target, filters, margin = 8) {
+    target.filters = filters;
+    if (!target.cacheCanvas) {
+      const bounds = target.getBounds();
+      target.cache(
+        bounds.x - margin,
+        bounds.y - margin,
+        bounds.width + margin * 2,
+        bounds.height + margin * 2
+      );
+    } else {
+      target.updateCache();
+    }
+  }
+  /**
+   * テキストオブジェクトのキャッシュと更新を行う。
+   * テキストに変化がない場合は処理をスキップする。
+   * @param {createjs.Text} target
+   * @param {string} value
+   * @param option オプション　marginはテキスト周囲のキャッシュのマージンサイズ colorはテキスト色
+   */
+  static cacheText(target, value, option) {
+    if (!target) return false;
+    //optionのデフォルト値を追加。
+    if (!option) {
+      option = {};
+    }
+    if (!option.margin) {
+      option.margin = 8;
+    }
+    if (!option.color) {
+      option.color = target.color;
+    }
+    //状態が同一か確認
+    let isSame = true;
+    const isSameString = target.text === value;
+    if (!isSameString) isSame = false;
+    if (target.color !== option.color) {
+      isSame = false;
+    }
+    if (isSame) {
+      //状態が全く同じなら更新をせずに終了
+      return false;
+    }
+    //文字とカラーの更新
+    target.text = value;
+    if (target.color !== option.color) {
+      target.color = option.color;
+    }
+    //すでにキャッシュ済みで同じ文字列を入力するならキャッシュの更新で終了
+    if (target.cacheCanvas && isSameString) {
+      target.updateCache();
+      return true;
+    }
+    //キャッシュのサイズ更新が必要な場合はアンキャッシュを行う。
+    //アンキャッシュ前にgetBoundsを呼ぶと、変更済みのサイズではなくキャッシュのバウンディングボックスが返ってくるため。
+    target.uncache();
+    const bounds = target.getBounds();
+    //空文字などサイズが計測不能な場合はキャッシュするのを諦めて処理を中断。
+    if (bounds === null || bounds === undefined) {
+      if (target.cacheCanvas) {
+        target.uncache();
+      }
+      return false;
+    }
+    //文言が異なる場合は再キャッシュ。
+    target.cache(
+      bounds.x - option.margin,
+      bounds.y - option.margin,
+      bounds.width + option.margin * 2,
+      bounds.height + option.margin * 2
+    );
+    return true;
+  }
+}
---
+++ example/main.js
@@ -0,0 +1,66 @@
+import { CreatejsCacheUtil } from "./createjs-text-cache.js";
+
+const onDomContentsLoaded = () => {
+  //FPSメーターの生成と配置
+  const stats = new Stats();
+  stats.showPanel(0);
+  stats.domElement.style.cssText =
+    "position:absolute; z-index:999; top:0; left:0;";
+
+  document.body.appendChild(stats.domElement);
+
+  // 生成する文字列の長さ
+  const l = 3;
+  // 生成する文字列に含める文字セット
+  const c = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cl = c.length;
+  let r = "";
+  for (let i = 0; i < l; i++) {
+    r += c[Math.floor(Math.random() * cl)];
+  }
+
+  //ステージ更新処理
+  const updateStage = () => {
+    if (texts) {
+      for (let text of texts) {
+        CreatejsCacheUtil.cacheText(text, r);
+      }
+    }
+    stats.begin();
+    stage.update();
+    stats.end();
+  };
+
+  //Create.jsのグローバル設定
+  createjs.Ticker.timingMode = createjs.Ticker.RAF;
+  createjs.Text.prototype.snapToPixel = false;
+
+  //stageの初期化
+  const canvas = document.getElementById("textCanvas");
+  canvas.width = 1280;
+  canvas.height = 480;
+  const stage = new createjs.Stage(canvas);
+  createjs.Ticker.on("tick", updateStage);
+
+  //Textオブジェクトの配置
+  const texts = [];
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 80; j++) {
+      const text = new createjs.Text("", "16px Meiryo", "#000");
+      text.x = i * 36;
+      text.y = j * 20;
+      CreatejsCacheUtil.cacheText(text, "TXT");
+      stage.addChild(text);
+      texts.push(text);
+    }
+  }
+};
+
+/**
+ * DOMContentLoaded以降に初期化処理を実行する
+ */
+if (document.readyState !== "loading") {
+  onDomContentsLoaded();
+} else {
+  document.addEventListener("DOMContentLoaded", onDomContentsLoaded);
+}
