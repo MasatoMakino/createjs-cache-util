a6d3dc0045a76ad3b4f42b4a69b45b523f1626a9
MasatoMakino <unagiinu.g@gmail.com>
2018-11-04 15:10:38 +0900

    デモページ用スクリプトのリファクタリング。

--- docs/demo/main.js
+++ docs/demo/main.js
@@ -1,6 +1,6 @@
 import { CreatejsCacheUtil } from "../../bin/createjs-text-cache";
 
-const onDomContentsLoaded = () => {
+const initStats = () => {
   //FPSメーターの生成と配置
   const stats = new Stats();
   stats.showPanel(0);
@@ -8,16 +8,39 @@ const onDomContentsLoaded = () => {
     "position:absolute; z-index:999; top:0; left:0;";
 
   document.body.appendChild(stats.domElement);
+  return stats;
+};
 
-  // 生成する文字列の長さ
-  const l = 3;
+const getRandomString = length => {
   // 生成する文字列に含める文字セット
   const c = "abcdefghijklmnopqrstuvwxyz0123456789";
   const cl = c.length;
-  let r = "";
-  for (let i = 0; i < l; i++) {
-    r += c[Math.floor(Math.random() * cl)];
+  let randomString = "";
+  for (let i = 0; i < length; i++) {
+    randomString += c[Math.floor(Math.random() * cl)];
   }
+  return randomString;
+};
+
+const initTexts = stage => {
+  const texts = [];
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 80; j++) {
+      const text = new createjs.Text("", "16px Meiryo", "#000");
+      text.x = i * 36;
+      text.y = j * 20;
+      CreatejsCacheUtil.cacheText(text, "TXT");
+      stage.addChild(text);
+      texts.push(text);
+    }
+  }
+  return texts;
+};
+
+const onDomContentsLoaded = () => {
+  //FPSメーターの生成と配置
+  const stats = initStats();
+  const r = getRandomString(3);
 
   //ステージ更新処理
   const updateStage = () => {
@@ -42,18 +65,7 @@ const onDomContentsLoaded = () => {
   const stage = new createjs.Stage(canvas);
   createjs.Ticker.on("tick", updateStage);
 
-  //Textオブジェクトの配置
-  const texts = [];
-  for (let i = 0; i < 100; i++) {
-    for (let j = 0; j < 80; j++) {
-      const text = new createjs.Text("", "16px Meiryo", "#000");
-      text.x = i * 36;
-      text.y = j * 20;
-      CreatejsCacheUtil.cacheText(text, "TXT");
-      stage.addChild(text);
-      texts.push(text);
-    }
-  }
+  const texts = initTexts(stage);
 };
 
 /**
