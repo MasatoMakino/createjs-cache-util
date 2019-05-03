3bfd0d27be52c381c2bbfcfd2d3b7c4f1a599220
MasatoMakino <unagiinu.g@gmail.com>
2019-05-03 18:09:04 +0900

    キャッシュオプションにスケールを追加。

--- src/createjs-text-cache.ts
+++ src/createjs-text-cache.ts
@@ -7,16 +7,18 @@ export class CreatejsCacheUtil {
    * @param {createjs.DisplayObject} target
    * @param {createjs.Filter[]} filters
    * @param {number} margin
+   * @param {number} scale
    */
   public static setFilter(
     target: DisplayObject,
     filters: createjs.Filter[],
-    margin: number = 8
+    margin: number = 8,
+    scale: number = 1
   ): void {
     target.filters = filters;
 
-    if (!target.cacheCanvas) {
-      this.refreshCache(target, margin);
+    if (!target.bitmapCache) {
+      this.refreshCache(target, margin, scale);
     } else {
       target.updateCache();
     }
@@ -40,16 +42,17 @@ export class CreatejsCacheUtil {
     if (!this.isNeedUpdate(target, value, option)) return;
 
     //文字とカラーの更新
+    const currentText = target.text;
     target.text = value;
     target.color = option.color;
 
     //すでにキャッシュ済みで同じ文字列を入力するならキャッシュの更新で終了
-    if (target.cacheCanvas && target.text === value) {
+    if (target.bitmapCache && currentText === value) {
       target.updateCache();
       return;
     }
 
-    this.refreshCache(target, option.margin);
+    this.refreshCache(target, option.margin, option.scale);
   }
 
   /**
@@ -58,22 +61,43 @@ export class CreatejsCacheUtil {
    *
    * @param {createjs.DisplayObject} target
    * @param {number} margin
+   * @param {number} scale
    */
-  private static refreshCache(target: DisplayObject, margin: number): void {
+  private static refreshCache(
+    target: DisplayObject,
+    margin: number,
+    scale: number
+  ): void {
     //キャッシュのサイズ更新が必要な場合はアンキャッシュを行う。
     //アンキャッシュ前にgetBoundsを呼ぶと、変更済みのサイズではなくキャッシュのバウンディングボックスが返ってくるため。
     target.uncache();
-    const bounds = target.getBounds();
+
+    const rect = this.getRect(target, margin);
 
     //targetが空文字などサイズが計測不能な場合はキャッシュするのを諦めて処理を中断。
-    if (bounds == null) return;
+    if (rect == null) return;
 
-    target.cache(
-      bounds.x - margin,
-      bounds.y - margin,
-      bounds.width + margin * 2,
-      bounds.height + margin * 2
-    );
+    target.cache(rect.x, rect.y, rect.width, rect.height, scale);
+  }
+
+  /**
+   * キャッシュ用の座標を取得。
+   * @param target
+   * @param margin
+   */
+  private static getRect(
+    target: DisplayObject,
+    margin: number
+  ): { x: number; y: number; width: number; height: number } {
+    const bounds = target.getBounds();
+    if (bounds == null) return null;
+
+    return {
+      x: bounds.x - margin,
+      y: bounds.y - margin,
+      width: bounds.width + margin * 2,
+      height: bounds.height + margin * 2
+    };
   }
 
   /**
@@ -91,11 +115,16 @@ export class CreatejsCacheUtil {
     option: CacheTextOption
   ): boolean {
     //キャッシュが行われていないなら強制的にキャッシュを更新。
-    if (!target.cacheCanvas) return true;
+    if (!target.bitmapCache) return true;
 
     //状態が同一か確認
     if (target.text !== value) return true;
     if (target.color !== option.color) return true;
+
+    //スケール値が存在し、かつ同一かを確認
+    const cacheScale = (target.bitmapCache as any).scale; //2019/05/03 bitmapCache.scaleプロパティは非公開である。将来的に取得できなくなる可能性がある。
+    if (cacheScale != null && cacheScale !== option.scale) return true;
+
     return false;
   }
 }
@@ -106,6 +135,7 @@ export class CreatejsCacheUtil {
 export class CacheTextOption {
   margin?: number;
   color?: string;
+  scale?: number; //キャッシュのスケール。指定された倍率のビットマップキャッシュが生成される。既定値1。
 
   /**
    * 不足している値をデフォルト値で埋める。
@@ -117,6 +147,7 @@ export class CacheTextOption {
     if (option == null) option = {};
     if (option.margin == null) option.margin = 8;
     if (!option.color) option.color = target.color;
+    if (option.scale == null) option.scale = 1;
 
     return option;
   }
